15d5a6ac54c4ae3fd1b71f60e76aa7c4e3f1cd87
Данил Гузеев <dannyellz@MacBook-Pro-Danil.local>
2022-09-01 10:44:19 +0400

    new changes

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import _ from "lodash"
 
 console.log(chalk.blue('Hello, Danil\n Nice to see you back!'));
 console.log(_.last(['one', 'two']));
+const fn   = (a, b) => {
+
+     const c = a + b; return c;
+   }
